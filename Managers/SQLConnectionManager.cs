a80ca1c8d2f89340e3862cd148dd58bd9e6df8df
schamrockt <k4yr225@gmail.com>
2025-05-08 00:40:26 +0300

    Add SQLConnectionManager class for database connection management

---
+++ Managers/SQLConnectionManager.cs
@@ -0,0 +1,38 @@
+﻿namespace dmyo_oop_final_assigment.Managers
+{
+	using System;
+	using System.Data.SqlClient;
+
+	public static class SqlConnectionManager
+	{
+		// Connection string
+		private static string connectionString = "Data Source=localhost;Initial Catalog=dmyo_oop_final_assigment;Integrated Security=True;Trust Server Certificate=True";
+
+		// Connection object
+		private static SqlConnection connection;
+
+
+		// Util method to open the connection
+		private static void OpenConnection()
+		{
+			if (connection == null)
+			{
+				connection = new SqlConnection(connectionString);
+			}
+
+			if (connection.State != System.Data.ConnectionState.Open)
+			{
+				connection.Open();
+			}
+		}
+
+		// Util method to close the connection
+		private static void CloseConnection()
+		{
+			if (connection != null && connection.State == System.Data.ConnectionState.Open)
+			{
+				connection.Close();
+			}
+		}
+	}
+}
