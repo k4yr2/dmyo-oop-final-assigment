ed9fb8ff737ccd2b076208b0197ed746b879b4fb
schamrockt <k4yr225@gmail.com>
2025-05-08 00:10:03 +0300

    Reorder SQL table creation to ensure WasteTypes is created before WasteRecords

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,14 +1,5 @@
 USE dmyo_oop_final_assigment;
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteTypes' AND type = 'U')
-BEGIN
-	CREATE TABLE WasteTypes (
-		id INT PRIMARY KEY IDENTITY(1,1),
-		name NVARCHAR(50) NOT NULL,
-		description NVARCHAR(500)
-	);
-END;
-
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteCollections' AND type = 'U')
 BEGIN
 	CREATE TABLE WasteCollections (
@@ -16,6 +7,15 @@ BEGIN
 		date DATETIME,
 		place NVARCHAR(255),
 		status NVARCHAR(50)
+	);
+END;
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteTypes' AND type = 'U')
+BEGIN
+	CREATE TABLE WasteTypes (
+		id INT PRIMARY KEY IDENTITY(1,1),
+		name NVARCHAR(50) NOT NULL,
+		description NVARCHAR(500)
 	);
 END;
 
