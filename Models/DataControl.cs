9be8ae662cf823dbcf1e3dd4ebfee2ab586f9cb1
schamrockt <k4yr225@gmail.com>
2025-05-08 13:12:16 +0300

    Add DataControl class for managing data objects in the UI

---
+++ Models/DataControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace dmyo_oop_final_assigment.Models
+{
+	public abstract class DataControl<TModel> : UserControl where TModel : class
+	{
+		private DataObject<TModel> m_object;
+
+		public DataObject<TModel> Object
+		{
+			get
+			{
+				return m_object;
+			}
+			set
+			{
+				m_object = value;
+			}
+		}
+	}
+}
