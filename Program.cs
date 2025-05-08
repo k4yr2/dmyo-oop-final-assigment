bbdf7f6c3eaa580d8d108cecb2b5d13cd5574dfb
schamrockt <k4yr225@gmail.com>
2025-05-08 10:07:27 +0300

    Implement WasteTypesForm, replacing NewWasteTypeForm in Program and updating project references

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace dmyo_oop_final_assigment
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new NewWasteTypeForm());
+			Application.Run(new WasteTypesForm());
 		}
 	}
 }
