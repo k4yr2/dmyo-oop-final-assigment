35e48728d265341842a082087f2cb1483c5229d2
schamrockt <k4yr225@gmail.com>
2025-05-08 12:06:26 +0300

    Add IDataQuery interface for querying SQLModel instances

---
+++ Providers/IDataQuery.cs
@@ -0,0 +1,10 @@
+﻿using dmyo_oop_final_assigment.Models;
+using System.Collections.Generic;
+
+namespace dmyo_oop_final_assigment.Providers
+{
+	internal interface IDataQuery
+	{
+		IEnumerable<SQLModel<TModel>> ReadAll<TModel>() where TModel : class;
+	}
+}
