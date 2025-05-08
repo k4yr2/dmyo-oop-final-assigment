93d9a0d3f292463c85c9c8ea7e3a4f4257d2e20b
schamrockt <k4yr225@gmail.com>
2025-05-08 13:04:59 +0300

    Add RepoManager class for centralized repository access

---
+++ Managers/RepoManager.cs
@@ -0,0 +1,24 @@
+﻿using dmyo_oop_final_assigment.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment.Managers
+{
+	public static class RepoManager
+	{
+		public static WasteTypeRepo m_wasteType = new WasteTypeRepo();
+
+		//
+
+		public static WasteTypeRepo WasteType
+		{
+			get
+			{
+				return m_wasteType;
+			}
+		}
+	}
+}
