38df2624c5e78dddc383bab849ce356449dc872a
schamrockt <k4yr225@gmail.com>
2025-05-08 23:01:05 +0300

    Add DataView enum and IDataControl interface for enhanced data management

---
+++ Models/DataView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment.Models
+{
+	public enum DataView
+	{
+		Show,
+		Blank,
+		Update,
+		Create
+	}
+}
---
+++ Providers/IDataControl.cs
@@ -0,0 +1,9 @@
+﻿using dmyo_oop_final_assigment.Models;
+
+namespace dmyo_oop_final_assigment.Providers
+{
+	public interface IDataControl<TModel> : IDataLink<TModel> where TModel : class
+	{
+		DataView View { get; set; }
+	}
+}
