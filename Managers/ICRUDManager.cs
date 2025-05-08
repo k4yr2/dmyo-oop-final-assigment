1127ee6177beb40f8beccb1eb3757139722dec8a
schamrockt <k4yr225@gmail.com>
2025-05-08 11:37:46 +0300

    Add ICRUDManager interface to define CRUD operations for models

---
+++ Managers/ICRUDManager.cs
@@ -0,0 +1,13 @@
+﻿namespace dmyo_oop_final_assigment.Managers
+{
+	public interface ICRUDManager<TModel>
+	{
+		TModel Create();
+
+		TModel Read(int id);
+
+		bool Update(int id);
+
+		bool Delete(int id);
+	}
+}
