af7eba28a6a266b6e5f7da68596fcca14834cf4c
schamrockt <k4yr225@gmail.com>
2025-05-08 13:13:26 +0300

    Refactor WasteTypeControl to inherit from DataControl for improved data management

--- Controls/WasteTypeControl.cs
+++ Controls/WasteTypeControl.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using dmyo_oop_final_assigment.Models;
 
 namespace dmyo_oop_final_assigment.Controls
 {
-	public partial class WasteTypeControl : UserControl
+	public partial class WasteTypeControl : DataControl<WasteType>
 	{
 		public WasteTypeControl()
 		{
