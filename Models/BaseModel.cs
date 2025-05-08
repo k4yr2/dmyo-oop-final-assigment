55cc6ea5337e19de094ff6cfead364e4675659bf
schamrockt <k4yr225@gmail.com>
2025-05-08 10:39:31 +0300

    Add BaseModel and WasteType classes to establish model hierarchy for waste management

---
+++ Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment.Models
+{
+	public abstract class BaseModel
+	{
+		public BaseModel(int id)
+		{
+			Id = id;
+		}
+
+		public int Id { get; set; }
+	}
+}
---
+++ Models/WasteType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment.Models
+{
+	public class WasteType : BaseModel
+	{
+		public WasteType(int id, string name, string description) : base(id)
+		{
+			Name = name;
+			Description = description;
+		}
+
+		public string Name { get; }
+
+		public string Description { get; }
+	}	
+}
