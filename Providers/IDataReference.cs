3780ceb9141ea67a9be369092e2334b6a1e27ca4
schamrockt <k4yr225@gmail.com>
2025-05-08 14:25:20 +0300

    Remove DataControl class and update project references; add IDataReference interface for improved data handling

---
+++ Providers/IDataReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace dmyo_oop_final_assigment.Models
+{
+	public interface IDataReference<TModel> where TModel : class
+	{
+		DataObject<TModel> Object { get; set; }
+	}
+}
