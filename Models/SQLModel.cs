9e5fbd46515b258aa7d0be7509cc4302ab0b9c43
schamrockt <k4yr225@gmail.com>
2025-05-08 11:41:32 +0300

    Add SQLModel class to manage model instances with unique identifiers

---
+++ Models/SQLModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment.Models
+{
+	public class SQLModel<TModel> where TModel : class
+	{
+		private int m_id;
+
+		private TModel m_model;
+
+		public SQLModel(int id, TModel model)
+		{
+			m_id = id;
+			m_model = model;
+		}
+
+
+		public int Id
+		{
+			get
+			{
+				return m_id;
+			}
+		}
+
+		public TModel Model
+		{
+			get
+			{
+				return m_model;
+			}
+			set
+			{
+				m_model = value;
+			}
+		}
+	}
+}
