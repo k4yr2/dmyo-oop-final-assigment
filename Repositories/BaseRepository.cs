2492b8494955a608f5fd8ecc539f6b491c2f31a1
schamrockt <k4yr225@gmail.com>
2025-05-08 12:17:26 +0300

    Add BaseRepository class for generic data management

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using dmyo_oop_final_assigment.Models;
+using dmyo_oop_final_assigment.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment.Repositories
+{
+	abstract class BaseRepository<TModel> : IDataCRUD<TModel>, IDataQuery<TModel> where TModel : class
+	{
+		public abstract SQLData<TModel> Create(TModel model);
+
+		public abstract SQLData<TModel> Read(int id);
+
+		public abstract bool Update(int id, TModel model);
+
+		public abstract bool Delete(int id);
+
+		public abstract IEnumerable<SQLData<TModel>> ReadAll();
+	}
+}
