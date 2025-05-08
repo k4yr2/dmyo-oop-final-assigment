a53e3b3afff2c9ee0f788e4fd589bea0318b59ac
schamrockt <k4yr225@gmail.com>
2025-05-08 10:01:38 +0300

    Refactor WasteTypesForm layout by removing unnecessary group box and adjusting control positions

--- Forms/WasteTypesForm.Designer.cs
+++ Forms/WasteTypesForm.Designer.cs
@@ -32,17 +32,15 @@
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.wt_group = new System.Windows.Forms.GroupBox();
 			this.wt_description = new System.Windows.Forms.Label();
 			this.wt_apply = new System.Windows.Forms.Button();
 			this.wt_delete = new System.Windows.Forms.Button();
-			this.wt_group.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// wt_name
 			// 
 			this.wt_name.AutoSize = true;
-			this.wt_name.Location = new System.Drawing.Point(9, 22);
+			this.wt_name.Location = new System.Drawing.Point(12, 9);
 			this.wt_name.Name = "wt_name";
 			this.wt_name.Size = new System.Drawing.Size(35, 13);
 			this.wt_name.TabIndex = 0;
@@ -50,14 +48,14 @@
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(89, 19);
+			this.textBox1.Location = new System.Drawing.Point(92, 6);
 			this.textBox1.Name = "textBox1";
 			this.textBox1.Size = new System.Drawing.Size(120, 20);
 			this.textBox1.TabIndex = 2;
 			// 
 			// richTextBox1
 			// 
-			this.richTextBox1.Location = new System.Drawing.Point(9, 64);
+			this.richTextBox1.Location = new System.Drawing.Point(12, 51);
 			this.richTextBox1.MaxLength = 250;
 			this.richTextBox1.Name = "richTextBox1";
 			this.richTextBox1.Size = new System.Drawing.Size(200, 80);
@@ -73,25 +71,10 @@
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Description";
 			// 
-			// wt_group
-			// 
-			this.wt_group.Controls.Add(this.wt_delete);
-			this.wt_group.Controls.Add(this.wt_apply);
-			this.wt_group.Controls.Add(this.richTextBox1);
-			this.wt_group.Controls.Add(this.textBox1);
-			this.wt_group.Controls.Add(this.wt_description);
-			this.wt_group.Controls.Add(this.wt_name);
-			this.wt_group.Location = new System.Drawing.Point(12, 12);
-			this.wt_group.Name = "wt_group";
-			this.wt_group.Size = new System.Drawing.Size(215, 181);
-			this.wt_group.TabIndex = 5;
-			this.wt_group.TabStop = false;
-			this.wt_group.Text = "Waste Type";
-			// 
 			// wt_description
 			// 
 			this.wt_description.AutoSize = true;
-			this.wt_description.Location = new System.Drawing.Point(6, 48);
+			this.wt_description.Location = new System.Drawing.Point(12, 35);
 			this.wt_description.Name = "wt_description";
 			this.wt_description.Size = new System.Drawing.Size(60, 13);
 			this.wt_description.TabIndex = 0;
@@ -99,7 +82,7 @@
 			// 
 			// wt_apply
 			// 
-			this.wt_apply.Location = new System.Drawing.Point(9, 150);
+			this.wt_apply.Location = new System.Drawing.Point(114, 137);
 			this.wt_apply.Name = "wt_apply";
 			this.wt_apply.Size = new System.Drawing.Size(98, 23);
 			this.wt_apply.TabIndex = 6;
@@ -109,7 +92,7 @@
 			// wt_delete
 			// 
 			this.wt_delete.Enabled = false;
-			this.wt_delete.Location = new System.Drawing.Point(111, 150);
+			this.wt_delete.Location = new System.Drawing.Point(10, 137);
 			this.wt_delete.Name = "wt_delete";
 			this.wt_delete.Size = new System.Drawing.Size(98, 23);
 			this.wt_delete.TabIndex = 7;
@@ -120,14 +103,18 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(239, 202);
-			this.Controls.Add(this.wt_group);
+			this.ClientSize = new System.Drawing.Size(223, 167);
+			this.Controls.Add(this.wt_delete);
+			this.Controls.Add(this.wt_apply);
 			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.wt_name);
+			this.Controls.Add(this.wt_description);
 			this.Name = "WasteTypesForm";
 			this.Text = "Waste Types";
-			this.wt_group.ResumeLayout(false);
-			this.wt_group.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -137,7 +124,6 @@
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.RichTextBox richTextBox1;
 		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.GroupBox wt_group;
 		private System.Windows.Forms.Label wt_description;
 		private System.Windows.Forms.Button wt_delete;
 		private System.Windows.Forms.Button wt_apply;
