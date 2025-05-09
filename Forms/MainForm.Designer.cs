0c758b2ed0e3ec2f220b80ad5fb0804958e82c1e
schamrockt <k4yr225@gmail.com>
2025-05-09 19:49:10 +0300

    Add MainForm layout with top, bottom, left, and content panels; update project file to include MainForm resources

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -28,12 +28,64 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.panel_top = new System.Windows.Forms.Panel();
+			this.panel_bottom = new System.Windows.Forms.Panel();
+			this.panel_left = new System.Windows.Forms.Panel();
+			this.panel_content = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// panel_top
+			// 
+			this.panel_top.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel_top.Location = new System.Drawing.Point(0, 0);
+			this.panel_top.Name = "panel_top";
+			this.panel_top.Size = new System.Drawing.Size(800, 60);
+			this.panel_top.TabIndex = 1;
+			// 
+			// panel_bottom
+			// 
+			this.panel_bottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel_bottom.Location = new System.Drawing.Point(0, 361);
+			this.panel_bottom.Name = "panel_bottom";
+			this.panel_bottom.Size = new System.Drawing.Size(800, 40);
+			this.panel_bottom.TabIndex = 2;
+			// 
+			// panel_left
+			// 
+			this.panel_left.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel_left.Location = new System.Drawing.Point(0, 60);
+			this.panel_left.Name = "panel_left";
+			this.panel_left.Size = new System.Drawing.Size(200, 301);
+			this.panel_left.TabIndex = 3;
+			// 
+			// panel_content
+			// 
+			this.panel_content.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel_content.Location = new System.Drawing.Point(200, 60);
+			this.panel_content.Name = "panel_content";
+			this.panel_content.Size = new System.Drawing.Size(600, 301);
+			this.panel_content.TabIndex = 4;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(800, 401);
+			this.Controls.Add(this.panel_content);
+			this.Controls.Add(this.panel_left);
+			this.Controls.Add(this.panel_bottom);
+			this.Controls.Add(this.panel_top);
+			this.Name = "MainForm";
 			this.Text = "MainForm";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel_top;
+		private System.Windows.Forms.Panel panel_bottom;
+		private System.Windows.Forms.Panel panel_left;
+		private System.Windows.Forms.Panel panel_content;
 	}
 }
