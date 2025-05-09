a2af46ffb935312673c16184e4f913fe75fb518e
schamrockt <k4yr225@gmail.com>
2025-05-09 19:31:27 +0300

    Remove WasteTypeControl and WasteTypesForm components, including associated resources and references, to streamline the project structure.

--- Providers/IDataControl.cs
+++
@@ -1,9 +0,0 @@
-﻿using dmyo_oop_final_assigment.Models;
-
-namespace dmyo_oop_final_assigment.Providers
-{
-	public interface IDataControl<TModel> : IDataLink<TModel> where TModel : class
-	{
-		DataView View { get; set; }
-	}
-}
