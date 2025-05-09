fec9282fa6383956377535c735d5a6b12af7bd7c
schamrockt <k4yr225@gmail.com>
2025-05-09 22:10:44 +0300

    Enhance MainForm layout by adding sidebar buttons for Wastes, Distribution, and Recycling; update panel colors for improved UI consistency

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -31,46 +31,106 @@ namespace dmyo_oop_final_assigment.Forms
 		private void InitializeComponent()
 		{
 			this.panel_banner = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
 			this.panel_statusbar = new System.Windows.Forms.Panel();
 			this.panel_sidebar = new System.Windows.Forms.Panel();
+			this.button_wastes = new System.Windows.Forms.Button();
 			this.panel_content = new System.Windows.Forms.Panel();
+			this.button_distribution = new System.Windows.Forms.Button();
+			this.button_recycling = new System.Windows.Forms.Button();
+			this.panel_banner.SuspendLayout();
+			this.panel_sidebar.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// panel_banner
 			// 
+			this.panel_banner.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+			this.panel_banner.Controls.Add(this.button1);
 			this.panel_banner.Dock = System.Windows.Forms.DockStyle.Top;
 			this.panel_banner.Location = new System.Drawing.Point(0, 0);
 			this.panel_banner.Name = "panel_banner";
 			this.panel_banner.Size = new System.Drawing.Size(800, 40);
 			this.panel_banner.TabIndex = 1;
-			this.panel_banner.BackColor = ColorTranslator.FromHtml("#2C3E50");
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(89)))), ((int)(((byte)(152)))));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(74, 0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(2, 2);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = false;
 			// 
 			// panel_statusbar
 			// 
+			this.panel_statusbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(165)))), ((int)(((byte)(166)))));
 			this.panel_statusbar.Dock = System.Windows.Forms.DockStyle.Bottom;
 			this.panel_statusbar.Location = new System.Drawing.Point(0, 360);
 			this.panel_statusbar.Name = "panel_statusbar";
 			this.panel_statusbar.Size = new System.Drawing.Size(800, 40);
 			this.panel_statusbar.TabIndex = 2;
-			this.panel_statusbar.BackColor = ColorTranslator.FromHtml("#95A5A6");
 			// 
 			// panel_sidebar
 			// 
+			this.panel_sidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+			this.panel_sidebar.Controls.Add(this.button_recycling);
+			this.panel_sidebar.Controls.Add(this.button_distribution);
+			this.panel_sidebar.Controls.Add(this.button_wastes);
 			this.panel_sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel_sidebar.ForeColor = System.Drawing.SystemColors.HighlightText;
 			this.panel_sidebar.Location = new System.Drawing.Point(0, 40);
 			this.panel_sidebar.Name = "panel_sidebar";
+			this.panel_sidebar.Padding = new System.Windows.Forms.Padding(10);
 			this.panel_sidebar.Size = new System.Drawing.Size(200, 320);
 			this.panel_sidebar.TabIndex = 3;
-			this.panel_sidebar.BackColor = ColorTranslator.FromHtml("#34495E");
+			// 
+			// button_wastes
+			// 
+			this.button_wastes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(89)))), ((int)(((byte)(152)))));
+			this.button_wastes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button_wastes.Location = new System.Drawing.Point(20, 20);
+			this.button_wastes.Margin = new System.Windows.Forms.Padding(10);
+			this.button_wastes.Name = "button_wastes";
+			this.button_wastes.Size = new System.Drawing.Size(160, 25);
+			this.button_wastes.TabIndex = 0;
+			this.button_wastes.Text = "Wastes";
+			this.button_wastes.UseVisualStyleBackColor = false;
 			// 
 			// panel_content
 			// 
+			this.panel_content.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
 			this.panel_content.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.panel_content.Location = new System.Drawing.Point(200, 40);
+			this.panel_content.Margin = new System.Windows.Forms.Padding(20);
 			this.panel_content.Name = "panel_content";
 			this.panel_content.Size = new System.Drawing.Size(600, 320);
 			this.panel_content.TabIndex = 4;
-			this.panel_content.BackColor = ColorTranslator.FromHtml("#ECF0F1");
+			// 
+			// button_distribution
+			// 
+			this.button_distribution.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(89)))), ((int)(((byte)(152)))));
+			this.button_distribution.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button_distribution.Location = new System.Drawing.Point(20, 65);
+			this.button_distribution.Margin = new System.Windows.Forms.Padding(10);
+			this.button_distribution.Name = "button_distribution";
+			this.button_distribution.Size = new System.Drawing.Size(160, 25);
+			this.button_distribution.TabIndex = 0;
+			this.button_distribution.Text = "Distribution";
+			this.button_distribution.UseVisualStyleBackColor = false;
+			// 
+			// button_recycling
+			// 
+			this.button_recycling.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(89)))), ((int)(((byte)(152)))));
+			this.button_recycling.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button_recycling.Location = new System.Drawing.Point(20, 110);
+			this.button_recycling.Margin = new System.Windows.Forms.Padding(10);
+			this.button_recycling.Name = "button_recycling";
+			this.button_recycling.Size = new System.Drawing.Size(160, 25);
+			this.button_recycling.TabIndex = 0;
+			this.button_recycling.Text = "Recycling ";
+			this.button_recycling.UseVisualStyleBackColor = false;
 			// 
 			// MainForm
 			// 
@@ -83,6 +143,8 @@ namespace dmyo_oop_final_assigment.Forms
 			this.Controls.Add(this.panel_banner);
 			this.Name = "MainForm";
 			this.Text = "MainForm";
+			this.panel_banner.ResumeLayout(false);
+			this.panel_sidebar.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -93,5 +155,9 @@ namespace dmyo_oop_final_assigment.Forms
 		private System.Windows.Forms.Panel panel_statusbar;
 		private System.Windows.Forms.Panel panel_sidebar;
 		private System.Windows.Forms.Panel panel_content;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button_wastes;
+		private System.Windows.Forms.Button button_recycling;
+		private System.Windows.Forms.Button button_distribution;
 	}
 }
