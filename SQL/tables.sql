9f61c39280e354133bd2ef423fa5869cd38c356b
schamrockt <k4yr225@gmail.com>
2025-05-09 22:36:43 +0300

    Fix table names in tables.sql for consistency with existing database structure

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,8 +1,8 @@
 USE dmyo_oop_final_assigment;
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteCategories' AND type = 'U')
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteCategory' AND type = 'U')
 BEGIN
-	CREATE TABLE WasteCategories (
+	CREATE TABLE WasteCategory (
 		id INT PRIMARY KEY IDENTITY(1,1),
 		name NVARCHAR(50) NOT NULL,
 		description NVARCHAR(500),
@@ -11,22 +11,22 @@ BEGIN
 	);
 END;
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteUnits' AND type = 'U')
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteUnit' AND type = 'U')
 BEGIN
-	CREATE TABLE WasteUnits (
+	CREATE TABLE WasteUnit (
 		id INT PRIMARY KEY IDENTITY(1,1),
 		name NVARCHAR(50) NOT NULL,
 		abbreviation  NVARCHAR(8)
 	);
 END;
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteTypes' AND type = 'U')
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'WasteType' AND type = 'U')
 BEGIN
-	CREATE TABLE WasteTypes (
+	CREATE TABLE WasteType (
 		id INT PRIMARY KEY IDENTITY(1,1),
 		name NVARCHAR(50) NOT NULL,
 		description NVARCHAR(500),
-		category INT FOREIGN KEY REFERENCES WasteCategories(id),
-		unit INT FOREIGN KEY REFERENCES WasteUnits(id),
+		category INT FOREIGN KEY REFERENCES WasteCategory(id),
+		unit INT FOREIGN KEY REFERENCES WasteUnit(id),
 	);
 END;
