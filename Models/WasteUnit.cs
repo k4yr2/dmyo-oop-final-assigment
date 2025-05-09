8a7d155a07252e0545a049374e500a624e4ca340
schamrockt <k4yr225@gmail.com>
2025-05-09 22:57:26 +0300

    Add WasteUnit model class to represent waste units in the application

---
+++ Models/WasteUnit.cs
@@ -0,0 +1,39 @@
+﻿namespace dmyo_oop_final_assigment.Models
+{
+	public class WasteUnit
+	{
+		private string m_name;
+
+		private string m_abbreviation;
+
+		public WasteUnit()
+		{
+
+		}
+
+
+		public string Name
+		{
+			get
+			{
+				return m_name;
+			}
+			set
+			{
+				m_name = value;
+			}
+		}
+
+		public string Abbreviation
+		{
+			get
+			{
+				return m_abbreviation;
+			}
+			set
+			{
+				m_abbreviation = value;
+			}
+		}
+	}
+}
