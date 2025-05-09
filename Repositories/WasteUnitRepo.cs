4e0ea08547e992cfc67833b8f2dfd15addafa968
schamrockt <k4yr225@gmail.com>
2025-05-09 22:59:00 +0300

    Add WasteUnitRepo class to manage WasteUnit data operations

---
+++ Repositories/WasteUnitRepo.cs
@@ -0,0 +1,27 @@
+﻿using dmyo_oop_final_assigment.Models;
+using System.Data.SqlClient;
+
+namespace dmyo_oop_final_assigment.Repositories
+{
+	public class WasteUnitRepo : BaseRepo<WasteUnit>
+	{
+		public override string Name => "WasteUnit";
+
+		public override string[] Params => new string[] { "name", "abbreviation" };
+
+		protected override void OnParameters(WasteUnit wasteType, SqlCommand command)
+		{
+			command.Parameters.AddWithValue("@name", wasteType.Name);
+			command.Parameters.AddWithValue("@abbreviation", wasteType.Abbreviation);
+		}
+
+		protected override WasteUnit OnModel(SqlDataReader reader)
+		{
+			return new WasteUnit()
+			{
+				Name = reader.GetString(1),
+				Abbreviation = reader.GetString(2)
+			};
+		}
+	}
+}
