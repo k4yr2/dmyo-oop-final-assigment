89837e0c2706296d9dadb992344670c27dac1598
k4yr2 <kyrzkaya@gmail.com>
2025-05-10 14:42:35 +0300

    Refactor DMYOGridControl to include a constructor accepting a DMYOGrid<DMYOModel> source and implement binding logic.

--- Controls/DMYOGridControl.cs
+++ Controls/DMYOGridControl.cs
@@ -1,12 +1,31 @@
-﻿using System.Windows.Forms;
+﻿using dmyo_oop_final_assigment.Grids;
+using dmyo_oop_final_assigment.Models;
+using System.Windows.Forms;
 
 namespace dmyo_oop_final_assigment.Controls
 {
 	public partial class DMYOGridControl : UserControl
 	{
-		public DMYOGridControl()
+		private DMYOGrid<DMYOModel> m_source;
+
+		public DMYOGridControl(DMYOGrid<DMYOModel> source)
 		{
 			InitializeComponent();
+			Bind(source);
+		}
+
+
+		public DMYOGrid<DMYOModel> Source
+		{
+			get
+			{
+				return m_source;
+			}
+		}
+
+		public void Bind(DMYOGrid<DMYOModel> source)
+		{
+			m_source = source;
 		}
 	}
 }
