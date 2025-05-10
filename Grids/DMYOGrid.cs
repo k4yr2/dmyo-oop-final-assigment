af6606aef8257a72ed425a98a0c7a21e87563d54
k4yr2 <kyrzkaya@gmail.com>
2025-05-10 12:59:06 +0300

    Add DMYOGrid abstract class for grid management functionality

---
+++ Grids/DMYOGrid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment.Grids
+{
+	public abstract class DMYOGrid<TModel> where TModel : class
+	{
+
+	}
+}
