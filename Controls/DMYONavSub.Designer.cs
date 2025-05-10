84eb72acb311c7257ad1ce923dfab9227fae9d1d
k4yr2 <kyrzkaya@gmail.com>
2025-05-10 23:14:11 +0300

    Add DMYONavSub control with designer and resource files

---
+++ Controls/DMYONavSub.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace dmyo_oop_final_assigment.Controls
+{
+	partial class DMYONavSub
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.bodyPanel = new System.Windows.Forms.Panel();
+			this.label = new System.Windows.Forms.Label();
+			this.button = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.bodyPanel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// bodyPanel
+			// 
+			this.bodyPanel.Controls.Add(this.label);
+			this.bodyPanel.Controls.Add(this.button);
+			this.bodyPanel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.bodyPanel.Location = new System.Drawing.Point(0, 0);
+			this.bodyPanel.Margin = new System.Windows.Forms.Padding(0);
+			this.bodyPanel.Name = "bodyPanel";
+			this.bodyPanel.Size = new System.Drawing.Size(207, 30);
+			this.bodyPanel.TabIndex = 0;
+			// 
+			// label
+			// 
+			this.label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label.Location = new System.Drawing.Point(30, 0);
+			this.label.Margin = new System.Windows.Forms.Padding(0);
+			this.label.Name = "label";
+			this.label.Size = new System.Drawing.Size(177, 30);
+			this.label.TabIndex = 2;
+			this.label.Text = "label1";
+			this.label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// button
+			// 
+			this.button.FlatAppearance.BorderSize = 0;
+			this.button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button.Location = new System.Drawing.Point(5, 5);
+			this.button.Margin = new System.Windows.Forms.Padding(5);
+			this.button.Name = "button";
+			this.button.Size = new System.Drawing.Size(20, 20);
+			this.button.TabIndex = 1;
+			this.button.Text = ">";
+			this.button.UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 30);
+			this.panel1.Margin = new System.Windows.Forms.Padding(0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(207, 86);
+			this.panel1.TabIndex = 1;
+			// 
+			// DMYONavSub
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.bodyPanel);
+			this.Name = "DMYONavSub";
+			this.Size = new System.Drawing.Size(207, 116);
+			this.bodyPanel.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel bodyPanel;
+		private System.Windows.Forms.Button button;
+		private System.Windows.Forms.Label label;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ Controls/DMYONavSub.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace dmyo_oop_final_assigment.Controls
+{
+	public partial class DMYONavSub : UserControl
+	{
+		public DMYONavSub()
+		{
+			InitializeComponent();
+		}
+	}
+}
