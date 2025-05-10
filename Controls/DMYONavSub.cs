e3d4d8aa307843da4cbdc42834ee9bdc5d9caa65
k4yr2 <kyrzkaya@gmail.com>
2025-05-10 23:21:36 +0300

    Add Title property to DMYONavSub control for customizable label text

--- Controls/DMYONavSub.cs
+++ Controls/DMYONavSub.cs
@@ -1,9 +1,24 @@
-﻿using System.Windows.Forms;
+﻿using System.ComponentModel;
+using System.Windows.Forms;
 
 namespace dmyo_oop_final_assigment.Controls
 {
 	public partial class DMYONavSub : UserControl
 	{
+		[Category("NavSub")]
+		[Browsable(true)]
+		public string Title
+		{
+			get
+			{
+				return label.Text;
+			}
+			set
+			{
+				label.Text = value;
+			}
+		}
+
 		public DMYONavSub()
 		{
 			InitializeComponent();
