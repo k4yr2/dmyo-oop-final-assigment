3c4b36ea6a57f1958a7fcf799f49c67ca51a5527
k4yr2 <kyrzkaya@gmail.com>
2025-05-10 13:03:49 +0300

    Refactor DMYOGrid class to remove unused namespaces and ensure proper DataTable handling

--- Grids/DMYOGrid.cs
+++ Grids/DMYOGrid.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 
 namespace dmyo_oop_final_assigment.Grids
 {
 	public abstract class DMYOGrid<TModel> where TModel : class
 	{
+		public abstract void Columns(DataTable table);
 
+		public abstract TModel ToModel(DataRow row);
 	}
 }
