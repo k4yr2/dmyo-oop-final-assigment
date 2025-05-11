c51a6033955a6c8d6f04c22fee7437e475124e2d
k4yr2 <kyrzkaya@gmail.com>
2025-05-11 16:29:50 +0300

    Remove Repo property from IDataCollection interfaces

--- Providers/IDataCollection.cs
+++ Providers/IDataCollection.cs
@@ -7,15 +7,11 @@ namespace dmyo_oop_final_assigment.Providers
 	{
 		string SelectQuery { get; }
 
-		IDMYOData[] Repo { get; }
-
 		IEnumerable<IDMYOData> Select();
 	}
 
 	public interface IDataCollection<TModel> : IDataCollection where TModel : DMYOModel
 	{
-		new IDMYOData<TModel>[] Repo { get; }
-
 		new IEnumerable<DMYOData<TModel>> Select();
 	}
 }
