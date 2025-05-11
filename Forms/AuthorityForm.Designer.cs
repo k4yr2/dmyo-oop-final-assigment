c2b8bdbc850c43bf25310a78933d15a8abec052f
k4yr2 <kyrzkaya@gmail.com>
2025-05-11 20:10:18 +0300

    Add AuthorityForm and associated resources for user authentication

---
+++ Forms/AuthorityForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace dmyo_oop_final_assigment.Forms
+{
+	partial class AuthorityForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.nameLabel = new System.Windows.Forms.Label();
+			this.nameBox = new System.Windows.Forms.TextBox();
+			this.passwordBox = new System.Windows.Forms.TextBox();
+			this.passwordLabel = new System.Windows.Forms.Label();
+			this.loginButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// nameLabel
+			// 
+			this.nameLabel.AutoSize = true;
+			this.nameLabel.Location = new System.Drawing.Point(30, 15);
+			this.nameLabel.Name = "nameLabel";
+			this.nameLabel.Size = new System.Drawing.Size(35, 13);
+			this.nameLabel.TabIndex = 0;
+			this.nameLabel.Text = "Name";
+			// 
+			// nameBox
+			// 
+			this.nameBox.Location = new System.Drawing.Point(71, 12);
+			this.nameBox.Name = "nameBox";
+			this.nameBox.Size = new System.Drawing.Size(100, 20);
+			this.nameBox.TabIndex = 1;
+			// 
+			// passwordBox
+			// 
+			this.passwordBox.Location = new System.Drawing.Point(71, 38);
+			this.passwordBox.Name = "passwordBox";
+			this.passwordBox.Size = new System.Drawing.Size(100, 20);
+			this.passwordBox.TabIndex = 1;
+			// 
+			// passwordLabel
+			// 
+			this.passwordLabel.AutoSize = true;
+			this.passwordLabel.Location = new System.Drawing.Point(12, 41);
+			this.passwordLabel.Name = "passwordLabel";
+			this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+			this.passwordLabel.TabIndex = 0;
+			this.passwordLabel.Text = "Password";
+			// 
+			// loginButton
+			// 
+			this.loginButton.Location = new System.Drawing.Point(15, 66);
+			this.loginButton.Name = "loginButton";
+			this.loginButton.Size = new System.Drawing.Size(157, 23);
+			this.loginButton.TabIndex = 2;
+			this.loginButton.Text = "Login";
+			this.loginButton.UseVisualStyleBackColor = true;
+			// 
+			// AuthorityForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(184, 101);
+			this.Controls.Add(this.loginButton);
+			this.Controls.Add(this.passwordBox);
+			this.Controls.Add(this.nameBox);
+			this.Controls.Add(this.passwordLabel);
+			this.Controls.Add(this.nameLabel);
+			this.Name = "AuthorityForm";
+			this.Text = "Authority";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label nameLabel;
+		private System.Windows.Forms.TextBox nameBox;
+		private System.Windows.Forms.TextBox passwordBox;
+		private System.Windows.Forms.Label passwordLabel;
+		private System.Windows.Forms.Button loginButton;
+	}
+}
