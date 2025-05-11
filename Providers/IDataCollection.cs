0f65a0994cc1520695e4d2965dd619d868e286a1
k4yr2 <kyrzkaya@gmail.com>
2025-05-11 16:27:30 +0300

    Add Repo property to IDataCollection interfaces for data access

--- Providers/IDataCollection.cs
+++ Providers/IDataCollection.cs
@@ -7,11 +7,15 @@ namespace dmyo_oop_final_assigment.Providers
 	{
 		string SelectQuery { get; }
 
+		IDMYOData[] Repo { get; }
+
 		IEnumerable<IDMYOData> Select();
 	}
 
 	public interface IDataCollection<TModel> : IDataCollection where TModel : DMYOModel
 	{
+		new IDMYOData<TModel>[] Repo { get; }
+
 		new IEnumerable<DMYOData<TModel>> Select();
 	}
 }
