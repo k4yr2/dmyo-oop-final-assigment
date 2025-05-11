28ac401cd2efc9c7b617ee2a4e70a20c8e2712af
k4yr2 <kyrzkaya@gmail.com>
2025-05-11 19:27:30 +0300

    Refactor DMYOGridControl layout by adding panelBar for improved structure and adjusting user panel positioning

--- Controls/DMYOGridControl.Designer.cs
+++ Controls/DMYOGridControl.Designer.cs
@@ -30,23 +30,24 @@
 		{
 			this.view = new System.Windows.Forms.DataGridView();
 			this.panelAdmin = new System.Windows.Forms.Panel();
-			this.panel = new System.Windows.Forms.Panel();
 			this.panelUser = new System.Windows.Forms.Panel();
 			this.searchBox = new System.Windows.Forms.TextBox();
 			this.refreshButton = new System.Windows.Forms.Button();
 			this.searchLabel = new System.Windows.Forms.Label();
+			this.panelBar = new System.Windows.Forms.Panel();
 			((System.ComponentModel.ISupportInitialize)(this.view)).BeginInit();
-			this.panel.SuspendLayout();
 			this.panelUser.SuspendLayout();
+			this.panelBar.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// view
 			// 
+			this.view.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
 			this.view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.view.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.view.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.view.Location = new System.Drawing.Point(0, 30);
 			this.view.Name = "view";
-			this.view.Size = new System.Drawing.Size(400, 214);
+			this.view.Size = new System.Drawing.Size(458, 147);
 			this.view.TabIndex = 1;
 			// 
 			// panelAdmin
@@ -55,45 +56,33 @@
 			this.panelAdmin.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.panelAdmin.Dock = System.Windows.Forms.DockStyle.Left;
 			this.panelAdmin.Location = new System.Drawing.Point(0, 0);
+			this.panelAdmin.Margin = new System.Windows.Forms.Padding(0);
 			this.panelAdmin.MaximumSize = new System.Drawing.Size(0, 30);
 			this.panelAdmin.MinimumSize = new System.Drawing.Size(120, 30);
 			this.panelAdmin.Name = "panelAdmin";
 			this.panelAdmin.Size = new System.Drawing.Size(120, 30);
 			this.panelAdmin.TabIndex = 2;
 			// 
-			// panel
-			// 
-			this.panel.Controls.Add(this.panelUser);
-			this.panel.Controls.Add(this.panelAdmin);
-			this.panel.Controls.Add(this.view);
-			this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.panel.Location = new System.Drawing.Point(0, 0);
-			this.panel.Name = "panel";
-			this.panel.Size = new System.Drawing.Size(400, 244);
-			this.panel.TabIndex = 0;
-			// 
 			// panelUser
 			// 
-			this.panelUser.AutoSize = true;
-			this.panelUser.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.panelUser.Controls.Add(this.searchLabel);
 			this.panelUser.Controls.Add(this.refreshButton);
 			this.panelUser.Controls.Add(this.searchBox);
-			this.panelUser.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.panelUser.Location = new System.Drawing.Point(120, 0);
+			this.panelUser.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panelUser.Location = new System.Drawing.Point(218, 0);
 			this.panelUser.Margin = new System.Windows.Forms.Padding(0);
 			this.panelUser.MaximumSize = new System.Drawing.Size(0, 30);
 			this.panelUser.MinimumSize = new System.Drawing.Size(240, 30);
 			this.panelUser.Name = "panelUser";
 			this.panelUser.Padding = new System.Windows.Forms.Padding(5);
-			this.panelUser.Size = new System.Drawing.Size(280, 30);
+			this.panelUser.Size = new System.Drawing.Size(240, 30);
 			this.panelUser.TabIndex = 3;
 			// 
 			// searchBox
 			// 
 			this.searchBox.Anchor = System.Windows.Forms.AnchorStyles.Right;
 			this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.searchBox.Location = new System.Drawing.Point(90, 5);
+			this.searchBox.Location = new System.Drawing.Point(50, 5);
 			this.searchBox.Margin = new System.Windows.Forms.Padding(0);
 			this.searchBox.Name = "searchBox";
 			this.searchBox.Size = new System.Drawing.Size(120, 20);
@@ -102,7 +91,7 @@
 			// refreshButton
 			// 
 			this.refreshButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
-			this.refreshButton.Location = new System.Drawing.Point(215, 5);
+			this.refreshButton.Location = new System.Drawing.Point(175, 5);
 			this.refreshButton.Margin = new System.Windows.Forms.Padding(5, 0, 0, 0);
 			this.refreshButton.Name = "refreshButton";
 			this.refreshButton.Size = new System.Drawing.Size(60, 20);
@@ -114,7 +103,7 @@
 			// searchLabel
 			// 
 			this.searchLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
-			this.searchLabel.Location = new System.Drawing.Point(41, 5);
+			this.searchLabel.Location = new System.Drawing.Point(1, 5);
 			this.searchLabel.Margin = new System.Windows.Forms.Padding(0);
 			this.searchLabel.Name = "searchLabel";
 			this.searchLabel.Size = new System.Drawing.Size(49, 20);
@@ -122,18 +111,30 @@
 			this.searchLabel.Text = "Search :";
 			this.searchLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
+			// panelBar
+			// 
+			this.panelBar.Controls.Add(this.panelUser);
+			this.panelBar.Controls.Add(this.panelAdmin);
+			this.panelBar.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panelBar.Location = new System.Drawing.Point(0, 0);
+			this.panelBar.MaximumSize = new System.Drawing.Size(0, 30);
+			this.panelBar.Name = "panelBar";
+			this.panelBar.Size = new System.Drawing.Size(458, 30);
+			this.panelBar.TabIndex = 1;
+			// 
 			// DMYOGridControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.panel);
+			this.Controls.Add(this.view);
+			this.Controls.Add(this.panelBar);
 			this.Name = "DMYOGridControl";
-			this.Size = new System.Drawing.Size(400, 244);
+			this.Size = new System.Drawing.Size(458, 177);
 			((System.ComponentModel.ISupportInitialize)(this.view)).EndInit();
-			this.panel.ResumeLayout(false);
-			this.panel.PerformLayout();
 			this.panelUser.ResumeLayout(false);
 			this.panelUser.PerformLayout();
+			this.panelBar.ResumeLayout(false);
+			this.panelBar.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -142,10 +143,10 @@
 
 		private System.Windows.Forms.DataGridView view;
 		private System.Windows.Forms.Panel panelAdmin;
-		private System.Windows.Forms.Panel panel;
 		private System.Windows.Forms.Panel panelUser;
 		private System.Windows.Forms.TextBox searchBox;
 		private System.Windows.Forms.Label searchLabel;
 		private System.Windows.Forms.Button refreshButton;
+		private System.Windows.Forms.Panel panelBar;
 	}
 }
