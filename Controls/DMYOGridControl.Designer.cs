ce16d8af7d9ffcb546f4afc76508edb3090a924d
k4yr2 <kyrzkaya@gmail.com>
2025-05-11 18:59:45 +0300

    Refactor DMYOGridControl layout by adding panels and adjusting DataGridView properties for improved UI structure

--- Controls/DMYOGridControl.Designer.cs
+++ Controls/DMYOGridControl.Designer.cs
@@ -28,51 +28,66 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
 			this.view = new System.Windows.Forms.DataGridView();
-			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.panelAdmin = new System.Windows.Forms.Panel();
+			this.panel = new System.Windows.Forms.Panel();
+			this.panelUser = new System.Windows.Forms.Panel();
 			((System.ComponentModel.ISupportInitialize)(this.view)).BeginInit();
+			this.panel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// view
 			// 
-			this.view.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.view.Dock = System.Windows.Forms.DockStyle.Bottom;
 			this.view.Location = new System.Drawing.Point(0, 30);
 			this.view.Name = "view";
-			this.view.Size = new System.Drawing.Size(400, 218);
-			this.view.TabIndex = 0;
-			this.view.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.view_CellEndEdit);
-			this.view.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.view_UserAddedRow);
-			this.view.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.view_UserDeletingRow);
+			this.view.Size = new System.Drawing.Size(400, 214);
+			this.view.TabIndex = 1;
 			// 
-			// contextMenuStrip1
+			// panelAdmin
 			// 
-			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			this.panelAdmin.AutoSize = true;
+			this.panelAdmin.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.panelAdmin.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panelAdmin.Location = new System.Drawing.Point(0, 0);
+			this.panelAdmin.MinimumSize = new System.Drawing.Size(120, 30);
+			this.panelAdmin.Name = "panelAdmin";
+			this.panelAdmin.Size = new System.Drawing.Size(120, 30);
+			this.panelAdmin.TabIndex = 2;
 			// 
-			// toolStrip1
+			// panel
 			// 
-			this.toolStrip1.AutoSize = false;
-			this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Table;
-			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(400, 30);
-			this.toolStrip1.TabIndex = 3;
-			this.toolStrip1.Text = "toolStrip1";
+			this.panel.Controls.Add(this.panelUser);
+			this.panel.Controls.Add(this.panelAdmin);
+			this.panel.Controls.Add(this.view);
+			this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel.Location = new System.Drawing.Point(0, 0);
+			this.panel.Name = "panel";
+			this.panel.Size = new System.Drawing.Size(400, 244);
+			this.panel.TabIndex = 0;
+			// 
+			// panelUser
+			// 
+			this.panelUser.AutoSize = true;
+			this.panelUser.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.panelUser.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelUser.Location = new System.Drawing.Point(120, 0);
+			this.panelUser.MinimumSize = new System.Drawing.Size(240, 30);
+			this.panelUser.Name = "panelUser";
+			this.panelUser.Size = new System.Drawing.Size(280, 30);
+			this.panelUser.TabIndex = 3;
 			// 
 			// DMYOGridControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.toolStrip1);
-			this.Controls.Add(this.view);
+			this.Controls.Add(this.panel);
 			this.Name = "DMYOGridControl";
 			this.Size = new System.Drawing.Size(400, 244);
 			((System.ComponentModel.ISupportInitialize)(this.view)).EndInit();
+			this.panel.ResumeLayout(false);
+			this.panel.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -80,7 +95,8 @@
 		#endregion
 
 		private System.Windows.Forms.DataGridView view;
-		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.Panel panelAdmin;
+		private System.Windows.Forms.Panel panel;
+		private System.Windows.Forms.Panel panelUser;
 	}
 }
