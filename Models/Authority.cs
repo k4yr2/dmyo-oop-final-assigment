44a4cc8ee168d5a8168dd726b95e16af20f83540
k4yr2 <kyrzkaya@gmail.com>
2025-05-11 19:51:16 +0300

    Add Authority model class with password property

---
+++ Models/Authority.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment.Models
+{
+	public class Authority : DMYOModel
+	{
+		private string m_password;
+
+		public Authority()
+		{
+
+		}
+
+
+		public string Password
+		{
+			get
+			{
+				return m_password;
+			}
+			set
+			{
+				m_password = value;
+			}
+		}
+	}
+}
