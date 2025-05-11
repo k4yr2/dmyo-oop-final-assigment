c3ae64eca406e31d81c698974a46ccd2bacd8c83
k4yr2 <kyrzkaya@gmail.com>
2025-05-11 20:18:35 +0300

    Implement login button click event handler in AuthorityForm for user authentication

--- Forms/AuthorityForm.cs
+++ Forms/AuthorityForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dmyo_oop_final_assigment.Managers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,26 @@ namespace dmyo_oop_final_assigment.Forms
 		public AuthorityForm()
 		{
 			InitializeComponent();
+		}
+
+		private void loginButton_Click(object sender, EventArgs e)
+		{
+			string name = nameBox.Text;
+			string password = passwordBox.Text;
+
+			var data =
+			TableManager.Authority.Select($"WHERE name = '{name}' and password = '{password}'").FirstOrDefault();
+		
+			if(data != null)
+			{
+				Program.Authority = data;
+				MessageBox.Show("Login successful", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				Close();
+			}
+			else
+			{
+				MessageBox.Show("Login failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 	}
 }
