4579a8b6c6ad32dc68bd73e283772f836c7f9a56
schamrockt <k4yr225@gmail.com>
2025-05-12 22:52:55 +0300

    Add login panel with name and password fields to MainForm

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -31,7 +31,14 @@
 			this.bannerPanel = new System.Windows.Forms.Panel();
 			this.bannerTitle = new System.Windows.Forms.Label();
 			this.startButton = new System.Windows.Forms.Button();
+			this.loginPanel = new System.Windows.Forms.Panel();
+			this.nameLabel = new System.Windows.Forms.Label();
+			this.nameBox = new System.Windows.Forms.TextBox();
+			this.passwordLabel = new System.Windows.Forms.Label();
+			this.passwordBox = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.bannerPanel.SuspendLayout();
+			this.loginPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// bannerPanel
@@ -66,17 +73,79 @@
 			this.startButton.UseVisualStyleBackColor = true;
 			this.startButton.Click += new System.EventHandler(this.startButton_Click);
 			// 
+			// loginPanel
+			// 
+			this.loginPanel.Controls.Add(this.button1);
+			this.loginPanel.Controls.Add(this.passwordBox);
+			this.loginPanel.Controls.Add(this.nameBox);
+			this.loginPanel.Controls.Add(this.passwordLabel);
+			this.loginPanel.Controls.Add(this.nameLabel);
+			this.loginPanel.Location = new System.Drawing.Point(10, 66);
+			this.loginPanel.Name = "loginPanel";
+			this.loginPanel.Size = new System.Drawing.Size(200, 92);
+			this.loginPanel.TabIndex = 2;
+			// 
+			// nameLabel
+			// 
+			this.nameLabel.Location = new System.Drawing.Point(0, 0);
+			this.nameLabel.Margin = new System.Windows.Forms.Padding(0);
+			this.nameLabel.Name = "nameLabel";
+			this.nameLabel.Padding = new System.Windows.Forms.Padding(5);
+			this.nameLabel.Size = new System.Drawing.Size(90, 30);
+			this.nameLabel.TabIndex = 3;
+			this.nameLabel.Text = "Name";
+			this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// nameBox
+			// 
+			this.nameBox.Location = new System.Drawing.Point(95, 5);
+			this.nameBox.Margin = new System.Windows.Forms.Padding(5);
+			this.nameBox.Name = "nameBox";
+			this.nameBox.Size = new System.Drawing.Size(100, 20);
+			this.nameBox.TabIndex = 4;
+			// 
+			// passwordLabel
+			// 
+			this.passwordLabel.Location = new System.Drawing.Point(0, 30);
+			this.passwordLabel.Margin = new System.Windows.Forms.Padding(0);
+			this.passwordLabel.Name = "passwordLabel";
+			this.passwordLabel.Padding = new System.Windows.Forms.Padding(5);
+			this.passwordLabel.Size = new System.Drawing.Size(90, 30);
+			this.passwordLabel.TabIndex = 3;
+			this.passwordLabel.Text = "Password";
+			this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// passwordBox
+			// 
+			this.passwordBox.Location = new System.Drawing.Point(95, 35);
+			this.passwordBox.Margin = new System.Windows.Forms.Padding(5);
+			this.passwordBox.Name = "passwordBox";
+			this.passwordBox.Size = new System.Drawing.Size(100, 20);
+			this.passwordBox.TabIndex = 4;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(95, 63);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 24);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 244);
+			this.Controls.Add(this.loginPanel);
 			this.Controls.Add(this.startButton);
 			this.Controls.Add(this.bannerPanel);
 			this.Name = "MainForm";
-			this.Text = "MainForm";
+			this.Text = "5";
 			this.bannerPanel.ResumeLayout(false);
 			this.bannerPanel.PerformLayout();
+			this.loginPanel.ResumeLayout(false);
+			this.loginPanel.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -86,5 +155,11 @@
 		private System.Windows.Forms.Panel bannerPanel;
 		private System.Windows.Forms.Label bannerTitle;
 		private System.Windows.Forms.Button startButton;
+		private System.Windows.Forms.Panel loginPanel;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox passwordBox;
+		private System.Windows.Forms.TextBox nameBox;
+		private System.Windows.Forms.Label passwordLabel;
+		private System.Windows.Forms.Label nameLabel;
 	}
 }
