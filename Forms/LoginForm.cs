7a578c067e113366ffe6aa399c28f2b4c321f1da
schamrockt <k4yr225@gmail.com>
2025-05-12 19:01:56 +0300

    Add LoginForm with designer and resource files

---
+++ Forms/LoginForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace dmyo_oop_final_assigment.Forms
+{
+	public partial class LoginForm : Form
+	{
+		public LoginForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
