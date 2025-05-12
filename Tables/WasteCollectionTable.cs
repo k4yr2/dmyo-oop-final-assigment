8118dcc7a4519978273e425121d34f46b2602c7e
schamrockt <k4yr225@gmail.com>
2025-05-12 22:26:20 +0300

    Add WasteCollectionTable class to manage waste collection data

---
+++ Tables/WasteCollectionTable.cs
@@ -0,0 +1,27 @@
+﻿using dmyo_oop_final_assigment.Models;
+using System.Data.SqlClient;
+
+namespace dmyo_oop_final_assigment.Tables
+{
+	public class WasteCollectionTable : DMYOTable<WasteCollection>
+	{
+		public override string Name => "WasteCollection";
+
+		public override string[] Params => new string[] { "date", "user"};
+
+		public override void SetParameters(WasteCollection collection, SqlCommand command)
+		{
+			command.Parameters.AddWithValue("@date", collection.Date);
+			command.Parameters.AddWithValue("@user", collection.User);
+		}
+
+		public override WasteCollection GetModel(SqlDataReader reader)
+		{
+			return new WasteCollection()
+			{
+				Date = reader.GetDateTime(1),
+				User = reader.GetInt32(2)
+			};
+		}
+	}
+}
