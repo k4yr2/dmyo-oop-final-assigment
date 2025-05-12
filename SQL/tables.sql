19168f05d9d67b37cdf98709211f8fe7f9157a37
schamrockt <k4yr225@gmail.com>
2025-05-12 22:25:57 +0300

    Refactor table definitions for consistency and clarity in WasteCategory, WasteType, WasteCollection, and WasteEntry

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -4,9 +4,9 @@ IF OBJECT_ID('User', 'U') IS NULL
 BEGIN
 	CREATE TABLE [User] (
 		id			INT PRIMARY		KEY IDENTITY(1,1),
+		role		INT				NOT NULL CHECK (role BETWEEN 0 AND 2),
 		name		NVARCHAR(100)	NOT NULL UNIQUE,
-		password	NVARCHAR(32)	NOT NULL,
-		role		INT				NOT NULL CHECK (role BETWEEN 0 AND 2)
+		password	NVARCHAR(32)	NOT NULL
 	);
 
 	INSERT INTO [User] (name, password, role)
@@ -27,9 +27,9 @@ BEGIN
 	CREATE TABLE [WasteCategory] (
 		id			INT				PRIMARY KEY IDENTITY(1,1),
 		name		NVARCHAR(50)	NOT NULL UNIQUE,
+		description NVARCHAR(500),
 		hazardLevel INT				DEFAULT 0 CHECK (hazardLevel BETWEEN 0 AND 5),
-		recyclable	BIT				DEFAULT(0),
-		description NVARCHAR(500)
+		recyclable	BIT				DEFAULT(0)
 	);
 END;
 
@@ -38,9 +38,9 @@ BEGIN
 	CREATE TABLE [WasteType] (
 		id			INT				PRIMARY KEY IDENTITY(1,1),
 		name		NVARCHAR(50)	NOT NULL UNIQUE,
-		unit		INT				FOREIGN KEY REFERENCES WasteUnit(id),
-		category	INT				FOREIGN KEY REFERENCES WasteCategory(id),
 		description NVARCHAR(500),
+		unit		INT				FOREIGN KEY REFERENCES WasteUnit(id),
+		category	INT				FOREIGN KEY REFERENCES WasteCategory(id)
 	);
 END;
 
@@ -48,8 +48,8 @@ IF OBJECT_ID('WasteCollection', 'U') IS NULL
 BEGIN
 	CREATE TABLE [WasteCollection] (
 		id			INT				PRIMARY KEY IDENTITY(1,1),
-		[user]		INT				FOREIGN KEY REFERENCES [User](id),
 		date		DATETIME		NOT NULL DEFAULT GETDATE(),
+		[user]		INT				FOREIGN KEY REFERENCES [User](id)
 	);
 END;
 
@@ -57,9 +57,9 @@ IF OBJECT_ID('WasteEntry', 'U') IS NULL
 BEGIN
 	CREATE TABLE [WasteEntry] (
 		id			INT				PRIMARY KEY IDENTITY(1,1),
-		type		INT				FOREIGN KEY REFERENCES WasteType(id) DEFAULT 0,
-		collection	INT				FOREIGN KEY REFERENCES WasteCollection(id) DEFAULT 0,
 		date		DATETIME		NOT NULL DEFAULT GETDATE(),
-		quantity	DECIMAL(10,2),
+		collection	INT				FOREIGN KEY REFERENCES WasteCollection(id) DEFAULT 0,
+		type		INT				FOREIGN KEY REFERENCES WasteType(id) DEFAULT 0,
+		quantity	DECIMAL(10, 2)
 	);
 END;
