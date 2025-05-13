123e9412d43d626e7e08372d5f4a61aa4ec2a6e2
k4yr2 <kyrzkaya@gmail.com>
2025-05-13 20:56:37 +0300

    Refactor DataManager to simplify connection management and improve resource handling

--- Managers/DataManager.cs
+++ Managers/DataManager.cs
@@ -1,16 +1,12 @@
-﻿using dmyo_oop_final_assigment.Models;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Reflection;
 
 namespace dmyo_oop_final_assigment.Managers
 {
 	internal static class DataManager
 	{
-		// Connection string
 		private static string connectionString = "Data Source=localhost;Initial Catalog=dmyo_oop_final_assigment;Integrated Security=True";
 
 
@@ -34,58 +30,35 @@ namespace dmyo_oop_final_assigment.Managers
 
 		public static void ExecuteCommand(string query, Action<SqlCommand> func)
 		{
-
-			SqlConnection connection = new SqlConnection(connectionString);
-
-			try
+			using (SqlConnection connection = new SqlConnection(connectionString))
 			{
-				connection.Open();
 				using (SqlCommand command = new SqlCommand(query, connection))
 				{
 					func.Invoke(command);
 				}
 			}
-			finally
-			{
-				connection.Close();
-			}
 		}
 
 		public static IEnumerable<T> ExecuteCommand<T>(string query, Func<SqlCommand, IEnumerable<T>> func)
 		{
-			SqlConnection connection = new SqlConnection(connectionString);
-
-			try
+			using (SqlConnection connection = new SqlConnection(connectionString))
 			{
-				connection.Open();
 				using (SqlCommand command = new SqlCommand(query, connection))
 				{
 					foreach (var item in func.Invoke(command))
 						yield return item;
 				}
 			}
-			finally
-			{
-				connection.Close();
-			}
 		}
 	
-
 		public static void FillTable(DataTable table, string query)
 		{
-			SqlConnection connection = new SqlConnection(connectionString);
-
-			try
+			using (SqlConnection connection = new SqlConnection(connectionString))
 			{
-				connection.Open();
 				using (SqlDataAdapter adapter = new SqlDataAdapter(query, connectionString))
 				{
 					adapter.Fill(table);
 				}
-			}
-			finally
-			{
-				connection.Close();
 			}
 		}
 	}
