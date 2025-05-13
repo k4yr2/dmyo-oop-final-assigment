c470fd59b32f1696020997c8bb613401d2eab9b0
k4yr2 <kyrzkaya@gmail.com>
2025-05-13 18:09:28 +0300

    Add DMYORepo class for generic data management and update project file

---
+++ DMYORepo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmyo_oop_final_assigment
+{
+	internal class DMYORepo<TModel>
+	{
+		private IDMYOTable<TModel> m_table;
+
+		private List<DMYOData<TModel>> m_list = new List<DMYOData<TModel>>();
+
+		public DMYORepo(IDMYOTable<TModel> table)
+		{
+			m_table = table;
+		}
+
+
+		public IDMYOTable<TModel> Table
+		{
+			get
+			{
+				return m_table;
+			}
+		}
+
+		public IEnumerable<DMYOData<TModel>> Datas
+		{
+			get
+			{
+				return m_list;
+			}
+		}
+
+
+		public void Refresh()
+		{
+			m_list.Clear();
+			m_list.AddRange(m_table.Select());
+		}
+	}
+}
