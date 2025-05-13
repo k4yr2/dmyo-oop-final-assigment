6f9ca96d34cfe75e59287c46cb1a6d0d4ade7544
k4yr2 <kyrzkaya@gmail.com>
2025-05-13 18:11:47 +0300

    Add RepoManager class for managing data repositories and update project file

---
+++ Managers/RepoManager.cs
@@ -0,0 +1,48 @@
+﻿using dmyo_oop_final_assigment.Models;
+
+namespace dmyo_oop_final_assigment.Managers
+{
+	static internal class RepoManager
+	{
+		static readonly DMYORepo<Person> m_person = new DMYORepo<Person>(TableManager.Person);
+
+		static readonly DMYORepo<WasteType> m_wasteType = new DMYORepo<WasteType>(TableManager.WasteType);
+
+		static readonly DMYORepo<WasteCategory> m_wasteCategory = new DMYORepo<WasteCategory>(TableManager.WasteCategory);
+
+		static readonly DMYORepo<WasteCollection> m_wasteCollection = new DMYORepo<WasteCollection>(TableManager.WasteCollection);
+
+
+		public static DMYORepo<Person> Person
+		{
+			get
+			{
+				return m_person;
+			}
+		}
+
+		public static DMYORepo<WasteType> WasteType
+		{
+			get
+			{
+				return m_wasteType;
+			}
+		}
+
+		public static DMYORepo<WasteCategory> WasteCategory
+		{
+			get
+			{
+				return m_wasteCategory;
+			}
+		}
+
+		public static DMYORepo<WasteCollection> WasteCollection
+		{
+			get
+			{
+				return m_wasteCollection;
+			}
+		}
+	}
+}
