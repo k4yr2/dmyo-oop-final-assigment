0be78f954191e19a764dbb32634d3f4ef28c6cee
k4yr2 <kyrzkaya@gmail.com>
2025-05-13 21:28:23 +0300

    Add WasteCollectionControl user control and associated resources

---
+++ Controls/WasteCollectionControl.cs
@@ -0,0 +1,30 @@
+﻿using dmyo_oop_final_assigment.Models;
+using System.Windows.Forms;
+
+namespace dmyo_oop_final_assigment.Controls
+{
+	public partial class WasteCollectionControl : UserControl
+	{
+		private WasteCollection m_collection = null;
+
+		public WasteCollectionControl(WasteCollection collection)
+		{
+			InitializeComponent();
+			m_collection = collection;
+		}
+
+
+		public WasteCollection Collection
+		{
+			get
+			{
+				return m_collection;
+			}
+			set
+			{
+				m_collection = value;
+				Refresh();
+			}
+		}
+	}
+}
