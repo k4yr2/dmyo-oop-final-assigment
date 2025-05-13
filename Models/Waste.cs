55780ddff529ca266931c8d2caf78c6ea0b5cdf3
k4yr2 <kyrzkaya@gmail.com>
2025-05-13 18:16:04 +0300

    Add Waste model class for waste management functionality

---
+++ Models/Waste.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace dmyo_oop_final_assigment.Models
+{
+	public class Waste
+	{
+		private DateTime m_date;
+
+		private int m_collection;
+
+		private int m_type;
+
+		private double m_quantity;
+
+		public Waste()
+		{
+
+		}
+
+
+		public DateTime Date
+		{
+			get
+			{
+				return m_date;
+			}
+			set
+			{
+				m_date = value;
+			}
+		}
+
+		public int Collection
+		{
+			get
+			{
+				return m_collection;
+			}
+			set
+			{
+				m_collection = value;
+			}
+		}
+
+		public int Type
+		{
+			get
+			{
+				return m_type;
+			}
+			set
+			{
+				m_type = value;
+			}
+		}
+
+		public double Quantity
+		{
+			get
+			{
+				return m_quantity;
+			}
+			set
+			{
+				m_quantity = value;
+			}
+		}
+	}
+}
