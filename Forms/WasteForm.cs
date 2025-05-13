8e4bf4222a1f8489c13b04f02caa22ec4a741394
k4yr2 <kyrzkaya@gmail.com>
2025-05-13 20:46:20 +0300

    Refactor WasteForm to improve variable naming and eliminate unused code

--- Forms/WasteForm.cs
+++ Forms/WasteForm.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Forms;
 using dmyo_oop_final_assigment.Controls;
 using dmyo_oop_final_assigment.Managers;
 using dmyo_oop_final_assigment.Models;
-using dmyo_oop_final_assigment.Providers;
 
 namespace dmyo_oop_final_assigment.Forms
 {
 	public partial class WasteForm : Form
 	{
-		private WasteControl m_data = null;
+		private WasteControl m_control = null;
 
 		private DMYOData<WasteCollection> m_collection = null;
 
@@ -25,20 +23,20 @@ namespace dmyo_oop_final_assigment.Forms
 
 		public WasteForm(DMYOData<WasteCollection> collection) : this()
 		{
-			m_data = null;
+			m_control = null;
 			m_collection = collection;
 			button.Text = "Add";
 		}
 
-		public WasteForm(WasteControl data) : this()
+		public WasteForm(WasteControl control) : this()
 		{
-			m_data = data;
-			m_collection = data.Collection;
+			m_control = control;
+			m_collection = control.Collection;
 			button.Text = "Update";
 		}
 
 
-		public WasteControl Data => m_data;
+		public WasteControl Control => m_control;
 
 
 		private void button_Click(object sender, EventArgs e)
@@ -57,13 +55,14 @@ namespace dmyo_oop_final_assigment.Forms
 				Quantity = quantity,
 			};
 
-			if (m_data == null)
+			if (m_control == null)
 			{
-				TableManager.Waste.Create(waste);
+				m_control = new WasteControl(m_collection, TableManager.Waste.Create(waste));
 			}
 			else
 			{
-				TableManager.Waste.Update(m_data.Data.Id, waste);
+				TableManager.Waste.Update(m_control.Data.Id, waste);
+				Control.Bind(Control.Data);
 			}
 
 			Close();
