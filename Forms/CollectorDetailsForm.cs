6d01114cc8446d384004fddb1c0bfc45c3ca651c
k4yr2 <kyrzkaya@gmail.com>
2025-05-17 17:41:06 +0300

    Refactor CollectorDetailsForm to include collection and waste properties for improved data management

--- Forms/CollectorDetailsForm.cs
+++ Forms/CollectorDetailsForm.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using dmyo_oop_final_assigment.Controls;
+using dmyo_oop_final_assigment.Pages;
 using System.Windows.Forms;
 
 namespace dmyo_oop_final_assigment.Forms
 {
 	public partial class CollectorDetailsForm : Form
 	{
-		public CollectorDetailsForm()
+		private CollectorCollection m_collection;
+
+		private CollectorWaste m_waste;
+
+		public CollectorDetailsForm(CollectorCollection collection) : this(collection, null)
+		{
+
+		}
+
+		public CollectorDetailsForm(CollectorCollection collection, CollectorWaste waste)
 		{
 			InitializeComponent();
+
+			m_collection = collection;
+			m_waste = waste;
+		}
+
+
+		public CollectorCollection Collection
+		{
+			get
+			{
+				return m_collection;
+			}
+		}
+
+		public CollectorWaste Waste
+		{
+			get
+			{
+				return m_waste;
+			}
 		}
 	}
 }
