fb9490f6c26838dbfb9ef639fae7f0aa9e72ee03
k4yr2 <kyrzkaya@gmail.com>
2025-05-17 20:11:19 +0300

    Add ContentContainer user control and associated resources for improved layout management

---
+++ Controls/ContentContainer.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace dmyo_oop_final_assigment.Controls
+{
+	public partial class ContentPanel : UserControl
+	{
+		public ContentPanel()
+		{
+			InitializeComponent();
+		}
+
+		public FlowLayoutPanel Panel
+		{
+			get
+			{
+				return panel;
+			}
+		}
+
+		public override void Refresh()
+		{
+			base.Refresh();
+
+			var controls = panel.Controls.OfType<Control>().ToArray();
+			for (int i = 0; i < controls.Length; i++)
+			{
+				controls[i].Width = panel.Width;
+
+				if (i % 2 == 0)
+				{
+					controls[i].BackColor = Color.Gainsboro;
+				}
+				else
+				{
+					controls[i].BackColor = Color.WhiteSmoke;
+				}
+			}
+		}
+
+
+		private void flowLayoutPanel1_ControlAdded(object sender, ControlEventArgs e)
+		{
+			Refresh();
+		}
+
+		private void panel_Resize(object sender, System.EventArgs e)
+		{
+			foreach (var control in panel.Controls.OfType<Control>())
+			{
+				control.Width = panel.Width;
+			}
+		}
+	}
+}
