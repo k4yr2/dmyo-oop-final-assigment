24dbe81d2887eb19e96dd8660a813b4079963ed3
k4yr2 <kyrzkaya@gmail.com>
2025-05-17 16:03:45 +0300

    Refactor CollectorWaste control to implement IDataLink interface and enhance data binding functionality

--- Controls/CollectorWaste.cs
+++ Controls/CollectorWaste.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using dmyo_oop_final_assigment.Models;
+using dmyo_oop_final_assigment.Providers;
 using System.Windows.Forms;
 
 namespace dmyo_oop_final_assigment.Controls
 {
-	public partial class CollectorWaste : UserControl
+	public partial class CollectorWaste : UserControl, IDataLink<Waste>
 	{
-		public CollectorWaste()
+		private DMYOData<Waste> m_source;
+
+		public CollectorWaste() : this(null)
+		{
+
+		}
+
+		public CollectorWaste(DMYOData<Waste> source)
 		{
 			InitializeComponent();
+			Bind(source);
+		}
+
+
+		public DMYOData<Waste> Source
+		{
+			get
+			{
+				return m_source;
+			}
+		}
+
+
+		public void Bind(DMYOData<Waste> source)
+		{
+			m_source = source;
+			Refresh();
+		}
+
+		public override void Refresh()
+		{
+			base.Refresh();
 		}
 	}
 }
