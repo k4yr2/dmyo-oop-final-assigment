f0303ab5d81fd6a02d2183b84702770f8a0816e0
k4yr2 <kyrzkaya@gmail.com>
2025-05-17 20:25:20 +0300

    Refactor CollectorDistributionItem to implement IDataLink and enhance data binding capabilities

--- Controls/CollectorDistributionItem.cs
+++ Controls/CollectorDistributionItem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using dmyo_oop_final_assigment.Models;
+using dmyo_oop_final_assigment.Providers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +12,56 @@ using System.Windows.Forms;
 
 namespace dmyo_oop_final_assigment.Controls
 {
-	public partial class CollectorDistributionItem : UserControl
+	public partial class CollectorDistributionItem : UserControl, IDataLink<WasteType>
 	{
-		public CollectorDistributionItem()
+		private CollectorDistribution m_distribution;
+
+		private DMYOData<WasteType> m_source;
+
+		private DMYOData<WasteCategory> m_category;
+
+		private DMYOData<WasteUnit> m_unit;
+
+		public CollectorDistributionItem(CollectorDistribution distribution) : this(distribution, null)
+		{
+
+		}
+
+		public CollectorDistributionItem(CollectorDistribution distribution, DMYOData<WasteType> source)
 		{
 			InitializeComponent();
+
+			m_distribution = distribution;
+			m_source = source;
+		}
+
+
+		public CollectorDistribution Distribution
+		{
+			get
+			{
+				return m_distribution;
+			}
+		}
+
+		public DMYOData<WasteType> Source
+		{
+			get
+			{
+				return m_source;
+			}
+		}
+
+
+		public void Bind(DMYOData<WasteType> source)
+		{
+			m_source = source;
+			Refresh();
+		}
+
+		public override void Refresh()
+		{
+			base.Refresh();
 		}
 	}
 }
