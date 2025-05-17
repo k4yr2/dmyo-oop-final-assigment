476081075d53653a19c412a8a0c81c1217ae0073
k4yr2 <kyrzkaya@gmail.com>
2025-05-17 14:47:03 +0300

    Update CollectorForm and CollectorIdlePage dimensions for improved layout and consistency

--- Pages/CollectorIdlePage.Designer.cs
+++ Pages/CollectorIdlePage.Designer.cs
@@ -46,7 +46,7 @@
 			// startButton
 			// 
 			this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.startButton.Location = new System.Drawing.Point(476, 63);
+			this.startButton.Location = new System.Drawing.Point(797, 63);
 			this.startButton.Name = "startButton";
 			this.startButton.Size = new System.Drawing.Size(100, 24);
 			this.startButton.TabIndex = 0;
@@ -62,9 +62,9 @@
 			this.navPanel.Controls.Add(this.statisticButton);
 			this.navPanel.Controls.Add(this.statisticLabel);
 			this.navPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.navPanel.Location = new System.Drawing.Point(0, 348);
+			this.navPanel.Location = new System.Drawing.Point(0, 360);
 			this.navPanel.Name = "navPanel";
-			this.navPanel.Size = new System.Drawing.Size(579, 90);
+			this.navPanel.Size = new System.Drawing.Size(900, 90);
 			this.navPanel.TabIndex = 5;
 			// 
 			// statisticLabel
@@ -76,7 +76,7 @@
 			this.statisticLabel.Margin = new System.Windows.Forms.Padding(0);
 			this.statisticLabel.Name = "statisticLabel";
 			this.statisticLabel.Padding = new System.Windows.Forms.Padding(0, 0, 110, 0);
-			this.statisticLabel.Size = new System.Drawing.Size(579, 30);
+			this.statisticLabel.Size = new System.Drawing.Size(900, 30);
 			this.statisticLabel.TabIndex = 0;
 			this.statisticLabel.Text = "See all your collection statistics";
 			this.statisticLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -84,7 +84,7 @@
 			// statisticButton
 			// 
 			this.statisticButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.statisticButton.Location = new System.Drawing.Point(476, 3);
+			this.statisticButton.Location = new System.Drawing.Point(797, 3);
 			this.statisticButton.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
 			this.statisticButton.Name = "statisticButton";
 			this.statisticButton.Size = new System.Drawing.Size(100, 24);
@@ -100,7 +100,7 @@
 			this.archiveLabel.Location = new System.Drawing.Point(0, 30);
 			this.archiveLabel.Name = "archiveLabel";
 			this.archiveLabel.Padding = new System.Windows.Forms.Padding(0, 0, 110, 0);
-			this.archiveLabel.Size = new System.Drawing.Size(579, 30);
+			this.archiveLabel.Size = new System.Drawing.Size(900, 30);
 			this.archiveLabel.TabIndex = 1;
 			this.archiveLabel.Text = "Browse collection from archive";
 			this.archiveLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -108,7 +108,7 @@
 			// archiveButton
 			// 
 			this.archiveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.archiveButton.Location = new System.Drawing.Point(476, 33);
+			this.archiveButton.Location = new System.Drawing.Point(797, 33);
 			this.archiveButton.Name = "archiveButton";
 			this.archiveButton.Size = new System.Drawing.Size(100, 24);
 			this.archiveButton.TabIndex = 2;
@@ -122,7 +122,7 @@
 			this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
 			this.topPanel.Location = new System.Drawing.Point(0, 0);
 			this.topPanel.Name = "topPanel";
-			this.topPanel.Size = new System.Drawing.Size(579, 40);
+			this.topPanel.Size = new System.Drawing.Size(900, 40);
 			this.topPanel.TabIndex = 7;
 			// 
 			// titleLabel
@@ -133,7 +133,7 @@
 			this.titleLabel.ForeColor = System.Drawing.Color.White;
 			this.titleLabel.Location = new System.Drawing.Point(0, 40);
 			this.titleLabel.Name = "titleLabel";
-			this.titleLabel.Size = new System.Drawing.Size(579, 60);
+			this.titleLabel.Size = new System.Drawing.Size(900, 60);
 			this.titleLabel.TabIndex = 8;
 			this.titleLabel.Text = "Collector Form";
 			this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -144,7 +144,7 @@
 			this.infoPanel.Location = new System.Drawing.Point(0, 100);
 			this.infoPanel.Margin = new System.Windows.Forms.Padding(0);
 			this.infoPanel.Name = "infoPanel";
-			this.infoPanel.Size = new System.Drawing.Size(579, 248);
+			this.infoPanel.Size = new System.Drawing.Size(900, 260);
 			this.infoPanel.TabIndex = 9;
 			// 
 			// idleButton
@@ -165,7 +165,7 @@
 			this.label1.Location = new System.Drawing.Point(0, 60);
 			this.label1.Name = "label1";
 			this.label1.Padding = new System.Windows.Forms.Padding(0, 0, 110, 0);
-			this.label1.Size = new System.Drawing.Size(579, 30);
+			this.label1.Size = new System.Drawing.Size(900, 30);
 			this.label1.TabIndex = 3;
 			this.label1.Text = "Start a new collection";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -179,7 +179,7 @@
 			this.Controls.Add(this.topPanel);
 			this.Controls.Add(this.navPanel);
 			this.Name = "CollectorIdlePage";
-			this.Size = new System.Drawing.Size(579, 438);
+			this.Size = new System.Drawing.Size(900, 450);
 			this.navPanel.ResumeLayout(false);
 			this.topPanel.ResumeLayout(false);
 			this.ResumeLayout(false);
