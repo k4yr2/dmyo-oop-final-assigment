ffe7d4b80bed7bf4bcc974a9e0c4beb05f9d2771
k4yr2 <kyrzkaya@gmail.com>
2025-05-17 14:12:35 +0300

    Add layout components to CollectorIdlePage for improved navigation and statistics display

--- Pages/CollectorIdlePage.Designer.cs
+++ Pages/CollectorIdlePage.Designer.cs
@@ -30,7 +30,13 @@
 		{
 			this.panel1 = new System.Windows.Forms.Panel();
 			this.startButton = new System.Windows.Forms.Button();
+			this.titleLabel = new System.Windows.Forms.Label();
+			this.navPanel = new System.Windows.Forms.Panel();
+			this.statisticLabel = new System.Windows.Forms.Label();
+			this.statisticButton = new System.Windows.Forms.Button();
+			this.infoPanel = new System.Windows.Forms.Panel();
 			this.panel1.SuspendLayout();
+			this.navPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// panel1
@@ -52,14 +58,75 @@
 			this.startButton.Text = "Start Collection";
 			this.startButton.UseVisualStyleBackColor = true;
 			// 
+			// titleLabel
+			// 
+			this.titleLabel.BackColor = System.Drawing.Color.LightSlateGray;
+			this.titleLabel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.titleLabel.ForeColor = System.Drawing.Color.White;
+			this.titleLabel.Location = new System.Drawing.Point(0, 0);
+			this.titleLabel.Name = "titleLabel";
+			this.titleLabel.Size = new System.Drawing.Size(582, 60);
+			this.titleLabel.TabIndex = 1;
+			this.titleLabel.Text = "Collector Form";
+			this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// navPanel
+			// 
+			this.navPanel.Controls.Add(this.statisticButton);
+			this.navPanel.Controls.Add(this.statisticLabel);
+			this.navPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.navPanel.Location = new System.Drawing.Point(0, 257);
+			this.navPanel.Name = "navPanel";
+			this.navPanel.Size = new System.Drawing.Size(582, 151);
+			this.navPanel.TabIndex = 5;
+			// 
+			// statisticLabel
+			// 
+			this.statisticLabel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.statisticLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.statisticLabel.Location = new System.Drawing.Point(0, 0);
+			this.statisticLabel.Margin = new System.Windows.Forms.Padding(0);
+			this.statisticLabel.Name = "statisticLabel";
+			this.statisticLabel.Padding = new System.Windows.Forms.Padding(110, 0, 0, 0);
+			this.statisticLabel.Size = new System.Drawing.Size(582, 30);
+			this.statisticLabel.TabIndex = 0;
+			this.statisticLabel.Text = "See all your collection statistics";
+			this.statisticLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// statisticButton
+			// 
+			this.statisticButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.statisticButton.Location = new System.Drawing.Point(5, 3);
+			this.statisticButton.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+			this.statisticButton.Name = "statisticButton";
+			this.statisticButton.Size = new System.Drawing.Size(100, 24);
+			this.statisticButton.TabIndex = 1;
+			this.statisticButton.Text = "Statistics";
+			this.statisticButton.UseVisualStyleBackColor = true;
+			// 
+			// infoPanel
+			// 
+			this.infoPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.infoPanel.Location = new System.Drawing.Point(0, 60);
+			this.infoPanel.Margin = new System.Windows.Forms.Padding(0);
+			this.infoPanel.Name = "infoPanel";
+			this.infoPanel.Size = new System.Drawing.Size(582, 197);
+			this.infoPanel.TabIndex = 6;
+			// 
 			// CollectorIdlePage
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.infoPanel);
+			this.Controls.Add(this.navPanel);
+			this.Controls.Add(this.titleLabel);
 			this.Controls.Add(this.panel1);
 			this.Name = "CollectorIdlePage";
 			this.Size = new System.Drawing.Size(582, 438);
 			this.panel1.ResumeLayout(false);
+			this.navPanel.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -68,5 +135,10 @@
 
 		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.Button startButton;
+		private System.Windows.Forms.Label titleLabel;
+		private System.Windows.Forms.Panel navPanel;
+		private System.Windows.Forms.Button statisticButton;
+		private System.Windows.Forms.Label statisticLabel;
+		private System.Windows.Forms.Panel infoPanel;
 	}
 }
