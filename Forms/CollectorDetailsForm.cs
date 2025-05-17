1ff885b65fb18397f28460109b4fdf556839dbe2
k4yr2 <kyrzkaya@gmail.com>
2025-05-17 17:45:58 +0300

    Refactor CollectorDetailsForm constructors to remove button text assignment and ensure proper initialization

--- Forms/CollectorDetailsForm.cs
+++ Forms/CollectorDetailsForm.cs
@@ -12,12 +12,12 @@ namespace dmyo_oop_final_assigment.Forms
 
 		public CollectorDetailsForm(CollectorCollection collection) : this(collection, null)
 		{
-			applyButton.Text = "Create";
+
 		}
 
 		public CollectorDetailsForm(CollectorWaste waste) : this(waste.Collection, waste)
 		{
-			applyButton.Text = "Update";
+
 		}
 
 		private CollectorDetailsForm(CollectorCollection collection, CollectorWaste waste)
@@ -26,6 +26,8 @@ namespace dmyo_oop_final_assigment.Forms
 
 			m_collection = collection;
 			m_waste = waste;
+
+			Refresh();
 		}
 
 
@@ -42,6 +44,21 @@ namespace dmyo_oop_final_assigment.Forms
 			get
 			{
 				return m_waste;
+			}
+		}
+
+
+		public override void Refresh()
+		{
+			base.Refresh();
+			
+			if(m_waste == null)
+			{
+				applyButton.Text = "Create";
+			}
+			else
+			{
+				applyButton.Text = "Update";
 			}
 		}
 	}
