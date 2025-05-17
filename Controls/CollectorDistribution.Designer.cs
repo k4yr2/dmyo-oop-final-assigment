0f23f6a09d9f5b9a6d38c4bb2b2e8e478a820c86
k4yr2 <kyrzkaya@gmail.com>
2025-05-17 19:16:07 +0300

    Add bottomPanel and navigation buttons to CollectorDistribution for improved user interaction

--- Controls/CollectorDistribution.Designer.cs
+++ Controls/CollectorDistribution.Designer.cs
@@ -31,8 +31,14 @@
 			this.topPanel = new System.Windows.Forms.Panel();
 			this.distributionLabel = new System.Windows.Forms.Label();
 			this.cancelButton = new System.Windows.Forms.Button();
+			this.bottomPanel = new System.Windows.Forms.Panel();
 			this.contentPanel = new System.Windows.Forms.FlowLayoutPanel();
+			this.firstButton = new System.Windows.Forms.Button();
+			this.previousButton = new System.Windows.Forms.Button();
+			this.lastButton = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
 			this.topPanel.SuspendLayout();
+			this.bottomPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// topPanel
@@ -72,23 +78,76 @@
 			this.cancelButton.Text = "X";
 			this.cancelButton.UseVisualStyleBackColor = false;
 			// 
+			// bottomPanel
+			// 
+			this.bottomPanel.BackColor = System.Drawing.Color.Silver;
+			this.bottomPanel.Controls.Add(this.button1);
+			this.bottomPanel.Controls.Add(this.lastButton);
+			this.bottomPanel.Controls.Add(this.previousButton);
+			this.bottomPanel.Controls.Add(this.firstButton);
+			this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.bottomPanel.Location = new System.Drawing.Point(0, 424);
+			this.bottomPanel.Name = "bottomPanel";
+			this.bottomPanel.Size = new System.Drawing.Size(568, 30);
+			this.bottomPanel.TabIndex = 3;
+			// 
 			// contentPanel
 			// 
 			this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.contentPanel.Location = new System.Drawing.Point(0, 40);
 			this.contentPanel.Name = "contentPanel";
-			this.contentPanel.Size = new System.Drawing.Size(568, 414);
-			this.contentPanel.TabIndex = 2;
+			this.contentPanel.Size = new System.Drawing.Size(568, 384);
+			this.contentPanel.TabIndex = 4;
+			// 
+			// firstButton
+			// 
+			this.firstButton.Location = new System.Drawing.Point(3, 3);
+			this.firstButton.Name = "firstButton";
+			this.firstButton.Size = new System.Drawing.Size(36, 24);
+			this.firstButton.TabIndex = 1;
+			this.firstButton.Text = "<<";
+			this.firstButton.UseVisualStyleBackColor = true;
+			// 
+			// previousButton
+			// 
+			this.previousButton.Location = new System.Drawing.Point(43, 3);
+			this.previousButton.Name = "previousButton";
+			this.previousButton.Size = new System.Drawing.Size(36, 24);
+			this.previousButton.TabIndex = 1;
+			this.previousButton.Text = "<";
+			this.previousButton.UseVisualStyleBackColor = true;
+			// 
+			// lastButton
+			// 
+			this.lastButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.lastButton.Location = new System.Drawing.Point(529, 3);
+			this.lastButton.Name = "lastButton";
+			this.lastButton.Size = new System.Drawing.Size(36, 24);
+			this.lastButton.TabIndex = 1;
+			this.lastButton.Text = ">>";
+			this.lastButton.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(487, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(36, 24);
+			this.button1.TabIndex = 1;
+			this.button1.Text = ">";
+			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// CollectorDistribution
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.Controls.Add(this.contentPanel);
+			this.Controls.Add(this.bottomPanel);
 			this.Controls.Add(this.topPanel);
 			this.Name = "CollectorDistribution";
 			this.Size = new System.Drawing.Size(568, 454);
 			this.topPanel.ResumeLayout(false);
+			this.bottomPanel.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -98,6 +157,11 @@
 		private System.Windows.Forms.Panel topPanel;
 		private System.Windows.Forms.Label distributionLabel;
 		private System.Windows.Forms.Button cancelButton;
+		private System.Windows.Forms.Panel bottomPanel;
 		private System.Windows.Forms.FlowLayoutPanel contentPanel;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button lastButton;
+		private System.Windows.Forms.Button previousButton;
+		private System.Windows.Forms.Button firstButton;
 	}
 }
