39d39d29e180b722162bd0c2b9f3dc66ee792930
schamrockt <k4yr225@gmail.com>
2025-05-21 19:56:03 +0300

    Refactor RecyclerStoringItem to implement IDataLink and add data binding functionality

--- Controls/RecyclerStoringItem.cs
+++ Controls/RecyclerStoringItem.cs
@@ -1,20 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using dmyo_oop_final_assigment.Models;
+using dmyo_oop_final_assigment.Providers;
 using System.Windows.Forms;
 
 namespace dmyo_oop_final_assigment.Controls
 {
-    public partial class RecyclerStoringItem : UserControl
+    public partial class RecyclerStoringItem : UserControl, IDataLink<WasteDistribution>
     {
-        public RecyclerStoringItem()
+        private RecyclerStoring m_host;
+
+        private DMYOData<WasteDistribution> m_source;
+
+        public RecyclerStoringItem(RecyclerStoring host) : this(host, null)
+        {
+
+        }
+
+        public RecyclerStoringItem(RecyclerStoring host, DMYOData<WasteDistribution> source)
         {
             InitializeComponent();
+
+            m_host = host;
+            Bind(source);
+        }
+
+
+        public RecyclerStoring Host
+        {
+            get
+            {
+                return m_host;
+            }
+        }
+
+        public DMYOData<WasteDistribution> Source
+        {
+            get
+            {
+                return m_source;
+            }
+        }
+
+
+        public void Bind(DMYOData<WasteDistribution> data)
+        {
+
         }
     }
 }
