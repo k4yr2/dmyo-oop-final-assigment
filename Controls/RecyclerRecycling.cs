e65f6d18c02026280ce1da697fc012928c490688
k4yr2 <kyrzkaya@gmail.com>
2025-05-22 07:30:37 +0300

    Add RecyclerRecycling user control and associated resources

---
+++ Controls/RecyclerRecycling.cs
@@ -0,0 +1,44 @@
+﻿using dmyo_oop_final_assigment.Forms;
+using dmyo_oop_final_assigment.Managers;
+using System.Windows.Forms;
+
+namespace dmyo_oop_final_assigment.Controls
+{
+    public partial class RecyclerRecycling : UserControl
+    {
+        private RecyclerForm m_form;
+
+        public RecyclerRecycling(RecyclerForm form)
+        {
+            InitializeComponent();
+            m_form = form;
+
+            Refresh();
+        }
+
+
+        public RecyclerForm Form
+        {
+            get
+            {
+                return m_form;
+            }
+        }
+
+        public FlowLayoutPanel Panel
+        {
+            get
+            {
+                return contentContainer.Panel;
+            }
+        }
+
+
+        public override void Refresh()
+        {
+            base.Refresh();
+
+            Panel.Controls.Clear();
+        }
+    }
+}
