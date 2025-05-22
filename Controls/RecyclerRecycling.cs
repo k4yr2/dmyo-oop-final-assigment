17334a0f89e5b16a70a53a6a095dc1ab0bfd6267
k4yr2 <kyrzkaya@gmail.com>
2025-05-22 07:35:26 +0300

    Refactor RecyclerRecycling to implement IDataLink interface and update constructor to accept DMYOData<WasteRecycling>

--- Controls/RecyclerRecycling.cs
+++ Controls/RecyclerRecycling.cs
@@ -1,19 +1,23 @@
 ﻿using dmyo_oop_final_assigment.Forms;
 using dmyo_oop_final_assigment.Managers;
+using dmyo_oop_final_assigment.Models;
+using dmyo_oop_final_assigment.Providers;
 using System.Windows.Forms;
 
 namespace dmyo_oop_final_assigment.Controls
 {
-    public partial class RecyclerRecycling : UserControl
+    public partial class RecyclerRecycling : UserControl, IDataLink<WasteRecycling>
     {
         private RecyclerForm m_form;
 
-        public RecyclerRecycling(RecyclerForm form)
+        private DMYOData<WasteRecycling> m_source;
+
+        public RecyclerRecycling(RecyclerForm form, DMYOData<WasteRecycling> source)
         {
             InitializeComponent();
             m_form = form;
 
-            Refresh();
+            Bind(source);
         }
 
 
@@ -33,12 +37,30 @@ namespace dmyo_oop_final_assigment.Controls
             }
         }
 
+        public DMYOData<WasteRecycling> Source
+        {
+            get
+            {
+                return m_source;
+            }
+        }
+
+        public void Bind(DMYOData<WasteRecycling> source)
+        {
+            m_source = source;
+            Refresh();
+        }
 
         public override void Refresh()
         {
             base.Refresh();
 
             Panel.Controls.Clear();
+
+            foreach (var product in TableManager.WasteProduct.GetProducts(m_source.Id))
+            {
+                //Panel.Controls.Add(new);
+            }
         }
     }
 }
